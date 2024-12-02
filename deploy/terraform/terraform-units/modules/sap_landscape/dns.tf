472216403d9cb956bad7b7b5f4d417348705a92e
Kimmo Forss <kimforss@microsoft.com>
2024-12-02 10:48:28 +0200

    Enhance key vault subnet configuration by adding missing security rules

--- deploy/terraform/terraform-units/modules/sap_landscape/dns.tf
+++ deploy/terraform/terraform-units/modules/sap_landscape/dns.tf
@@ -1,3 +1,11 @@
+
+#######################################4#######################################8
+#                                                                              #
+#                                Network links                                 #
+#                                                                              #
+#######################################4#######################################8
+
+
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_sap" {
   provider                             = azurerm.dnsmanagement
   count                                = local.use_Azure_native_DNS && var.use_private_endpoint && var.dns_settings.register_virtual_network_to_dns ? 1 : 0
@@ -40,13 +48,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vnet_sap_file" {
   registration_enabled                 = false
 }
 
-data "azurerm_private_dns_zone" "file" {
-  provider                             = azurerm.privatelinkdnsmanagement
-  count                                = var.dns_settings.register_storage_accounts_keyvaults_with_dns ? 1 : 0
-  name                                 = var.dns_settings.dns_zone_names.file_dns_zone_name
-  resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
-}
-
 resource "azurerm_private_dns_zone_virtual_network_link" "storage" {
   provider                             = azurerm.privatelinkdnsmanagement
   count                                = local.use_Azure_native_DNS  && var.use_private_endpoint ? 1 : 0
@@ -65,71 +66,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "storage" {
   private_dns_zone_name                = var.dns_settings.dns_zone_names.blob_dns_zone_name
   virtual_network_id                   = azurerm_virtual_network.vnet_sap[0].id
 }
-
-data "azurerm_private_dns_zone" "storage" {
-  provider                             = azurerm.privatelinkdnsmanagement
-  count                                = var.dns_settings.register_storage_accounts_keyvaults_with_dns ? 1 : 0
-  name                                 = var.dns_settings.dns_zone_names.blob_dns_zone_name
-  resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
-}
-
-
-resource "azurerm_private_dns_a_record" "witness_storage" {
-  provider                             = azurerm.privatelinkdnsmanagement
-  count                                = var.dns_settings.register_storage_accounts_keyvaults_with_dns ? 0 : 0
-  name                                 = lower(local.witness_storageaccount_name)
-  zone_name                            = var.dns_settings.dns_zone_names.blob_dns_zone_name
-  resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
-  ttl                                  = 3600
-  records                              = [azurerm_private_endpoint.witness_storage[count.index].private_service_connection[0].private_ip_address]
-
-  tags                                 = var.tags
-}
-
-
-resource "azurerm_private_dns_a_record" "storage_bootdiag" {
-  provider                             = azurerm.privatelinkdnsmanagement
-  count                                = var.dns_settings.register_storage_accounts_keyvaults_with_dns ? 0 : 0
-  name                                 = lower(local.storageaccount_name)
-
-  zone_name                            = var.dns_settings.dns_zone_names.blob_dns_zone_name
-  resource_group_name                  = local.resource_group_exists ? (
-                                           data.azurerm_resource_group.resource_group[0].name) : (
-                                           azurerm_resource_group.resource_group[0].name
-                                         )
-  ttl                                  = 3600
-  records                              = [azurerm_private_endpoint.storage_bootdiag[count.index].private_service_connection[0].private_ip_address]
-  tags                                 = var.tags
-}
-
-
-data "azurerm_private_dns_zone" "keyvault" {
-  provider                             = azurerm.privatelinkdnsmanagement
-  count                                = var.dns_settings.register_storage_accounts_keyvaults_with_dns ? 1 : 0
-  name                                 = var.dns_settings.dns_zone_names.vault_dns_zone_name
-  resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
-}
-
-# Duplicate code, the private endpoint deployment performs the DNS registration
-# resource "azurerm_private_dns_a_record" "keyvault" {
-#   provider                             = azurerm.privatelinkdnsmanagement
-#   count                                = local.use_Azure_native_DNS && var.use_private_endpoint ?  0 : 0
-#   name                                 = lower(
-#                                            format("%s", local.user_keyvault_name)
-#                                          )
-#   zone_name                            = var.dns_settings.dns_zone_names.vault_dns_zone_name
-#   resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
-#   ttl                                  = 10
-#   records                              = [
-#                                            length(var.keyvault_private_endpoint_id) > 0 ? (
-#                                              data.azurerm_private_endpoint_connection.kv_user[0].private_service_connection[0].private_ip_address) : (
-#                                              azurerm_private_endpoint.kv_user[0].private_service_connection[0].private_ip_address
-#                                            )
-#                                          ]
-#   tags                                 = var.tags
-
-# }
-
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vault" {
   provider                             = azurerm.privatelinkdnsmanagement
@@ -172,21 +108,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vault" {
 #   tags                                 = var.tags
 # }
 
-data "azurerm_private_dns_a_record" "transport" {
-  provider                             = azurerm.privatelinkdnsmanagement
-  count                                = var.create_transport_storage && var.use_private_endpoint && length(var.transport_private_endpoint_id) > 0 ? 1 : 0
-  name                                 = replace(
-                                           lower(
-                                             format("%s", local.landscape_shared_transport_storage_account_name)
-                                           ),
-                                           "/[^a-z0-9]/",
-                                           ""
-                                         )
-  zone_name                            = var.dns_settings.dns_zone_names.file_dns_zone_name
-  resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
-}
-
-
 # resource "azurerm_private_dns_a_record" "install" {
 #   provider                             = azurerm.privatelinkdnsmanagement
 #   count                                = var.use_private_endpoint && local.use_Azure_native_DNS && local.use_AFS_for_shared && length(var.install_private_endpoint_id) == 0 ? 1 : 0
@@ -212,6 +133,40 @@ data "azurerm_private_dns_a_record" "transport" {
 # }
 
 
+#######################################4#######################################8
+#                                                                              #
+#                                 DNS records                                  #
+#                                                                              #
+#######################################4#######################################8
+
+# resource "azurerm_private_dns_a_record" "witness_storage" {
+#   provider                             = azurerm.privatelinkdnsmanagement
+#   count                                = var.dns_settings.register_storage_accounts_keyvaults_with_dns ? 0 : 0
+#   name                                 = lower(local.witness_storageaccount_name)
+#   zone_name                            = var.dns_settings.dns_zone_names.blob_dns_zone_name
+#   resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
+#   ttl                                  = 3600
+#   records                              = [azurerm_private_endpoint.witness_storage[count.index].private_service_connection[0].private_ip_address]
+
+#   tags                                 = var.tags
+# }
+
+
+# resource "azurerm_private_dns_a_record" "storage_bootdiag" {
+#   provider                             = azurerm.privatelinkdnsmanagement
+#   count                                = var.dns_settings.register_storage_accounts_keyvaults_with_dns ? 0 : 0
+#   name                                 = lower(local.storageaccount_name)
+
+#   zone_name                            = var.dns_settings.dns_zone_names.blob_dns_zone_name
+#   resource_group_name                  = local.resource_group_exists ? (
+#                                            data.azurerm_resource_group.resource_group[0].name) : (
+#                                            azurerm_resource_group.resource_group[0].name
+#                                          )
+#   ttl                                  = 3600
+#   records                              = [azurerm_private_endpoint.storage_bootdiag[count.index].private_service_connection[0].private_ip_address]
+#   tags                                 = var.tags
+# }
+
 data "azurerm_private_dns_a_record" "install" {
   provider                             = azurerm.privatelinkdnsmanagement
   count                                = var.use_private_endpoint && length(var.install_private_endpoint_id) > 0 ? 1 : 0
@@ -223,5 +178,65 @@ data "azurerm_private_dns_a_record" "install" {
                                           ""
                                         )
   zone_name                            = var.dns_settings.dns_zone_names.file_dns_zone_name
+  resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
+}
+
+data "azurerm_private_dns_a_record" "transport" {
+  provider                             = azurerm.privatelinkdnsmanagement
+  count                                = var.create_transport_storage && var.use_private_endpoint && length(var.transport_private_endpoint_id) > 0 ? 1 : 0
+  name                                 = replace(
+                                           lower(
+                                             format("%s", local.landscape_shared_transport_storage_account_name)
+                                           ),
+                                           "/[^a-z0-9]/",
+                                           ""
+                                         )
+  zone_name                            = var.dns_settings.dns_zone_names.file_dns_zone_name
+  resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
+}
+
+# Duplicate code, the private endpoint deployment performs the DNS registration
+# resource "azurerm_private_dns_a_record" "keyvault" {
+#   provider                             = azurerm.privatelinkdnsmanagement
+#   count                                = local.use_Azure_native_DNS && var.use_private_endpoint ?  0 : 0
+#   name                                 = lower(
+#                                            format("%s", local.user_keyvault_name)
+#                                          )
+#   zone_name                            = var.dns_settings.dns_zone_names.vault_dns_zone_name
+#   resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
+#   ttl                                  = 10
+#   records                              = [
+#                                            length(var.keyvault_private_endpoint_id) > 0 ? (
+#                                              data.azurerm_private_endpoint_connection.kv_user[0].private_service_connection[0].private_ip_address) : (
+#                                              azurerm_private_endpoint.kv_user[0].private_service_connection[0].private_ip_address
+#                                            )
+#                                          ]
+#   tags                                 = var.tags
+
+# }
+
+#######################################4#######################################8
+#                                                                              #
+#                                   DNS zones                                  #
+#                                                                              #
+#######################################4#######################################
+data "azurerm_private_dns_zone" "file" {
+  provider                             = azurerm.privatelinkdnsmanagement
+  count                                = var.dns_settings.register_storage_accounts_keyvaults_with_dns ? 1 : 0
+  name                                 = var.dns_settings.dns_zone_names.file_dns_zone_name
+  resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
+}
+
+data "azurerm_private_dns_zone" "storage" {
+  provider                             = azurerm.privatelinkdnsmanagement
+  count                                = var.dns_settings.register_storage_accounts_keyvaults_with_dns ? 1 : 0
+  name                                 = var.dns_settings.dns_zone_names.blob_dns_zone_name
+  resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
+}
+
+data "azurerm_private_dns_zone" "keyvault" {
+  provider                             = azurerm.privatelinkdnsmanagement
+  count                                = var.dns_settings.register_storage_accounts_keyvaults_with_dns ? 1 : 0
+  name                                 = var.dns_settings.dns_zone_names.vault_dns_zone_name
   resource_group_name                  = var.dns_settings.privatelink_dns_resourcegroup_name
 }
